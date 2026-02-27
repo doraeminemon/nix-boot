2c2c34017ef9d99846d5c6e869b439c9242871b4
Tyson Do <doraeminemon@gmail.com>
2026-02-27 23:11:31 +0700

    Add a few more notes

--- test-bash-script.sh
+++ test-bash-script.sh
@@ -1,6 +1,9 @@
 sudo apt install -y chromium-browser mise fish starship kitty flatpak bleachbit fastfetch eza fzf zoxide curl git lazygit docker.io hw-probe
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install flathub -y com.vscodium.codium com.usebruno.Bruno
 curl -fsSL https://get.pnpm.io/install.sh | sh
+// ansible
+// encryption for main drive
+// bleach bit
 // setting up fisher
 // optionally install deno
 // install envelope
